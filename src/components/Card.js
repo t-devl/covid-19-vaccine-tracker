e45f23d7b1d6354b06d0530f7b1e7cc957b9fba5
t-devl <79603858+t-devl@users.noreply.github.com>
2021-03-31 20:44:47 +0100

    Add thousands separator to vaccination numbers

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,19 +5,25 @@ export default function Card({ data }) {
     <div className="card">
       <h3 className="card__name">{data.name}</h3>
       <p className="card__partially-vaccinated">
-        Partially vaccinated: {data.peoplePartiallyVaccinated}{" "}
-        {data.population
-          ? `(${(
-              (data.peoplePartiallyVaccinated / data.population) *
-              100
-            ).toFixed(2)}%)`
-          : ""}
+        {data.peoplePartiallyVaccinated.toLocaleString()} partially vaccinated{" "}
+        <span className="card__percentage">
+          {data.population
+            ? `(${(
+                (data.peoplePartiallyVaccinated / data.population) *
+                100
+              ).toFixed(2)}%)`
+            : ""}
+        </span>
       </p>
       <p className="card__fully-vaccinated">
-        Fully vaccinated: {data.peopleVaccinated}{" "}
-        {data.population
-          ? `(${((data.peopleVaccinated / data.population) * 100).toFixed(2)}%)`
-          : ""}
+        {data.peopleVaccinated.toLocaleString()} fully vaccinated{" "}
+        <span className="card__percentage">
+          {data.population
+            ? `(${((data.peopleVaccinated / data.population) * 100).toFixed(
+                2
+              )}%)`
+            : ""}
+        </span>
       </p>
     </div>
   );
