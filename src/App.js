de010f211ed87f2493879f2175eade415741dc19
t-devl <79603858+t-devl@users.noreply.github.com>
2021-04-02 12:16:01 +0100

    Store vaccination data in localStorage for an hour

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import CardsContainer from "./components/CardsContainer";
 import Filter from "./components/Filter";
 
 function App() {
-  const [filterOption, setFilterOption] = useState("Sovereign State");
+  const [filterOption, setFilterOption] = useState("");
   const [sovereignStates, setSovereignStates] = useState([]);
   const [unitedKingdom, setUnitedKingdom] = useState([]);
   const [europeanUnion, setEuropeanUnion] = useState([]);
@@ -13,98 +13,121 @@ function App() {
   const [displayedData, setDisplayedData] = useState([]);
 
   useEffect(() => {
-    fetch("https://covid-api.mmediagroup.fr/v1/vaccines")
-      .then((res) => res.json())
-      .then((data) => {
-        let unitedKingdom = [];
-        let europeanUnion = [];
-        let sovereignStates = [];
-        let continents = [];
-        let world = [];
+    if (
+      JSON.parse(localStorage.getItem("covidVaccineData")) &&
+      Date.now() -
+        JSON.parse(localStorage.getItem("covidVaccineData")).timestamp <
+        60 * 60 * 1000
+    ) {
+      let data = JSON.parse(localStorage.getItem("covidVaccineData"));
+      setSovereignStates([...data.sovereignStates]);
+      setUnitedKingdom([...data.unitedKingdom]);
+      setEuropeanUnion([...data.europeanUnion]);
+      setContinents([...data.continents]);
+      setWorld([...data.world]);
+      setFilterOption("Sovereign state");
+    } else {
+      fetch("https://covid-api.mmediagroup.fr/v1/vaccines")
+        .then((res) => res.json())
+        .then((data) => {
+          let unitedKingdom = [];
+          let europeanUnion = [];
+          let sovereignStates = [];
+          let continents = [];
+          let world = [];
 
-        Object.keys(data).forEach((key) => {
-          switch (key) {
-            case "England":
-            case "Northern Ireland":
-            case "Scotland":
-            case "Wales":
-              unitedKingdom.push({
-                name: key,
-                vaccinesAdministered: data[key].All.administered,
-                peopleVaccinated: data[key].All.people_partially_vaccinated,
-                peoplePartiallyVaccinated: data[key].All.people_vaccinated,
-                population: data[key].All.population,
-                continent: data[key].All.continent,
-                location: data[key].All.location,
-                lastUpdated: data[key].All.updated,
-              });
-              break;
-            case "European Union":
-              europeanUnion.push({
-                name: key,
-                administered: data[key].All.administered,
-                peopleVaccinated: data[key].All.people_partially_vaccinated,
-                peoplePartiallyVaccinated: data[key].All.people_vaccinated,
-                lastUpdated: data[key].All.updated,
-                population: data[key].All.population,
-              });
-              break;
-            case "Europe":
-            case "North America":
-            case "South America":
-            case "Oceania":
-            case "Asia":
-            case "Africa":
-              continents.push({
-                name: key,
-                administered: data[key].All.administered,
-                peopleVaccinated: data[key].All.people_partially_vaccinated,
-                peoplePartiallyVaccinated: data[key].All.people_vaccinated,
-                lastUpdated: data[key].All.updated,
-              });
-              break;
-            case "World":
-              world.push({
-                name: key,
-                administered: data[key].All.administered,
-                peopleVaccinated: data[key].All.people_partially_vaccinated,
-                peoplePartiallyVaccinated: data[key].All.people_vaccinated,
-                lastUpdated: data[key].All.updated,
-                population: data["Global"].All.population,
-              });
-              break;
-            case "Global":
-              break;
-            default:
-              sovereignStates.push({
-                name: key,
-                vaccinesAdministered: data[key].All.administered,
-                peopleVaccinated: data[key].All.people_partially_vaccinated,
-                peoplePartiallyVaccinated: data[key].All.people_vaccinated,
-                population: data[key].All.population,
-                continent: data[key].All.continent,
-                location: data[key].All.location,
-                lastUpdated: data[key].All.updated,
-              });
-              break;
-          }
+          Object.keys(data).forEach((key) => {
+            switch (key) {
+              case "England":
+              case "Northern Ireland":
+              case "Scotland":
+              case "Wales":
+                unitedKingdom.push({
+                  name: key,
+                  vaccinesAdministered: data[key].All.administered,
+                  peopleVaccinated: data[key].All.people_partially_vaccinated,
+                  peoplePartiallyVaccinated: data[key].All.people_vaccinated,
+                  population: data[key].All.population,
+                  continent: data[key].All.continent,
+                  location: data[key].All.location,
+                  lastUpdated: data[key].All.updated,
+                });
+                break;
+              case "European Union":
+                europeanUnion.push({
+                  name: key,
+                  administered: data[key].All.administered,
+                  peopleVaccinated: data[key].All.people_partially_vaccinated,
+                  peoplePartiallyVaccinated: data[key].All.people_vaccinated,
+                  lastUpdated: data[key].All.updated,
+                  population: data[key].All.population,
+                });
+                break;
+              case "Europe":
+              case "North America":
+              case "South America":
+              case "Oceania":
+              case "Asia":
+              case "Africa":
+                continents.push({
+                  name: key,
+                  administered: data[key].All.administered,
+                  peopleVaccinated: data[key].All.people_partially_vaccinated,
+                  peoplePartiallyVaccinated: data[key].All.people_vaccinated,
+                  lastUpdated: data[key].All.updated,
+                });
+                break;
+              case "World":
+                world.push({
+                  name: key,
+                  administered: data[key].All.administered,
+                  peopleVaccinated: data[key].All.people_partially_vaccinated,
+                  peoplePartiallyVaccinated: data[key].All.people_vaccinated,
+                  lastUpdated: data[key].All.updated,
+                  population: data["Global"].All.population,
+                });
+                break;
+              case "Global":
+                break;
+              default:
+                sovereignStates.push({
+                  name: key,
+                  vaccinesAdministered: data[key].All.administered,
+                  peopleVaccinated: data[key].All.people_partially_vaccinated,
+                  peoplePartiallyVaccinated: data[key].All.people_vaccinated,
+                  population: data[key].All.population,
+                  continent: data[key].All.continent,
+                  location: data[key].All.location,
+                  lastUpdated: data[key].All.updated,
+                });
+                break;
+            }
+          });
+          return {
+            sovereignStates,
+            unitedKingdom,
+            europeanUnion,
+            continents,
+            world,
+          };
+        })
+        .then((arrays) => {
+          setSovereignStates([...arrays.sovereignStates]);
+          setUnitedKingdom([...arrays.unitedKingdom]);
+          setEuropeanUnion([...arrays.europeanUnion]);
+          setContinents([...arrays.continents]);
+          setWorld([...arrays.world]);
+          setFilterOption("Sovereign state");
+
+          localStorage.setItem(
+            "covidVaccineData",
+            JSON.stringify({
+              ...arrays,
+              timestamp: Date.now(),
+            })
+          );
         });
-        return {
-          sovereignStates,
-          unitedKingdom,
-          europeanUnion,
-          continents,
-          world,
-        };
-      })
-      .then((arrays) => {
-        setSovereignStates([...arrays.sovereignStates]);
-        setUnitedKingdom([...arrays.unitedKingdom]);
-        setEuropeanUnion([...arrays.europeanUnion]);
-        setContinents([...arrays.continents]);
-        setWorld([...arrays.world]);
-        setDisplayedData([...arrays.sovereignStates]);
-      });
+    }
   }, []);
 
   useEffect(() => {
